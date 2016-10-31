ce4e0dbb5f81d6465849f9263adfdb0217d9a30c
Lyuda Lyska <lucy.daehyun@gmail.com>
2016-10-31 18:25:41 +0200

    Создана ветка branch1 к 1 заданию 4 модуля. Вывести все цифры введённого числа слева направо.

--- src/rin2016/module4/ShowNumbers.java
+++ src/rin2016/module4/ShowNumbers.java
@@ -1,10 +1,29 @@
 package rin2016.module4;
+import java.util.Scanner;
 
 public class ShowNumbers {
 
 	public static void main(String[] args) {
-		System.out.println("1 2 3 4 5 6");
+		//Вывести цифры введённого числа по порядку
+		Scanner s = new Scanner(System.in);
+		System.out.print("Введите число, которое мы разобьем на цифры: ");
+		int numberFromUser = s.nextInt();
+		
+		if (numberFromUser < 10) {
+			System.out.println("В числе " + numberFromUser + " одна цифра.");
+		} else if (numberFromUser >= 10) {
+			//Находим количество цифр в введенном числе
+			int numberDigits = (int) Math.log10(numberFromUser)+1; 
+			int digitNumber = 0;
+			
+			while (numberDigits != 0) {
+				int firstDigit = numberFromUser /(int) Math.pow(10, --numberDigits);
+				firstDigit = firstDigit % 10;
 
+				System.out.println(++digitNumber + "-я цифра числа " + 
+							numberFromUser + " = " + firstDigit);
+			}
+		}
+		s.close();
 	}
-
 }
