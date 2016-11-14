6ca0f3c6dffa8d1bd5ebc50a4c98cf90dd9962ab
Lyuda Lyska <lucy.daehyun@gmail.com>
2016-11-14 14:27:49 +0200

    из класса Point удалено поле класса - distance, его нахождение вынесено в метод.

--- src/rin2016/module8/sortPoints/Point.java
+++ src/rin2016/module8/sortPoints/Point.java
@@ -5,20 +5,17 @@ public class Point {
 	private int x;
 	private int y;
 	
-	private int distance;
 
 	public Point() {
 		name = "";
 		x = 0;
 		y = 0;
-		distance = 0;
 	}
 
 	public Point(final String name, final int x, final int y) {
 		setName(name);
 		setX(x);
 		setY(y);
-		setDistance();
 	}
 	
 	public void setName(final String name) {
@@ -27,22 +24,18 @@ public class Point {
 
 	public void setX(final int x) {
 		this.x = x;
-		setDistance();
 	}
 
 	public void setY(final int y) {
 		this.y = y;
-		setDistance();
 	}
 	
 	/*
-	 * Расстояние не задаётся вручную, высчитывает при каждом изменении X и Y.
-	 * 
 	 * Формула для нахождения расстояние от точки до начала координат: 
 	 * D = sqrt(x^2 + y^2)
 	 */
-	private void setDistance () {
-		this.distance = (int)Math.sqrt(Math.pow(this.x, 2)+Math.pow(this.y, 2));
+	public int getDistance () {
+		return (int)Math.sqrt(Math.pow(this.x, 2)+Math.pow(this.y, 2));
 	}
 
 	public String getName() {
@@ -55,9 +48,5 @@ public class Point {
 
 	public int getY() {
 		return y;
-	}
-	
-	public int getDistance() {
-		return distance;
 	}
 }
