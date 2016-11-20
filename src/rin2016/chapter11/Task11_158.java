a1c7379dadb650706bef9a20379b0402f75c7250
Lyuda Lyska <lucy.daehyun@gmail.com>
2016-11-20 15:23:35 +0200

    Изменено: При удалении элементов размер массива не изменяется. Массив изменяет размер после выхода из массива.

--- src/rin2016/chapter11/Task11_158.java
+++ src/rin2016/chapter11/Task11_158.java
@@ -15,17 +15,21 @@ public class Task11_158 {
 		System.out.println("Наш изначальный массив: \n" 
 				+ Arrays.toString(array));
 		
-		for (int i = 0; i < array.length; i++) {
-			for (int j = i+1; j < array.length; j++) {
+		int counterRemove = 0;
+		for (int i = 0; i < array.length-counterRemove; i++) {
+			for (int j = i+1; j < array.length-counterRemove; j++) {
 				if (array[i] == array[j]) {
-					array = removeElementWithArray(array, j);
+					removeElementWithArray(array, j);
 					//Ставим дикримент для того, чтобы цикл при удалении 
 					//элемента не пропустил два повторяющихся числа подряд
 					--j;
+					++counterRemove;
 				}
 			} 
 		}
-		
+		if (counterRemove > 0) {
+			array = copyArray(array, array.length-counterRemove);
+		}
 		System.out.println("\nМассив после удаления повторяющихся элементов: \n" 
 				+ Arrays.toString(array));
 	}
@@ -42,19 +46,18 @@ public class Task11_158 {
 	/*
 	 * Удаляет из массива елемент по индексу
 	*/
-	public static int[] removeElementWithArray(final int[] array, final int index) {
+	public static void removeElementWithArray(final int[] array, final int index) {
 		for (int i = index; i < array.length-1; i++) {
 			array[i] = array[i+1];
 		}
-		return copyArray(array, array.length-1);
 	}
 	
 	/*
 	 * Создаём новый массив (размер передаём в аргументе),
 	 * копируем данные из старого массива в новый и возвращаем его
 	*/
-	public static int[] copyArray(final int[] array, final int size) {
-		int[] temp = new int[size];
+	public static int[] copyArray(final int[] array, final int newSize) {
+		int[] temp = new int[newSize];
 		for (int i = 0; i < temp.length; i++) {
 			temp[i] = array[i];
 		}
