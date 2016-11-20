5c50e4d6564798be7ef962972200179562b16700
Lyuda Lyska <lucy.daehyun@gmail.com>
2016-11-20 12:50:52 +0200

    Изменён метод swap(). Вместо замены значений он меняет местами объекты по ссылкам.
    Изменён метод sort(), под изменения в методе swap().

--- src/rin2016/module8/sortPoints/SortPoints.java
+++ src/rin2016/module8/sortPoints/SortPoints.java
@@ -6,32 +6,27 @@ public class SortPoints {
 		final int SIZE = 5;
 		Point[] points = new Point[SIZE];
 		points[0] = new Point("A", -4, 6);
-		points[1] = new Point("B", 9, 4);
+		points[1] = new Point("B", 9, 7);
 		points[2] = new Point("C", 6, -2);
-		points[3] = new Point("D", -9, 5);
+		points[3] = new Point("D", -9, 3);
 		points[4] = new Point("E", 1, -3);
 		
 		sort(points);
 		show(points);
 	}
 	
-	public static void swap(final Point first, final Point second) {
-		Point temp = new Point(first.getName(), first.getX(), first.getY());
-		
-		first.setName(second.getName());
-		first.setX(second.getX());
-		first.setY(second.getY());
-		
-		second.setName(temp.getName());
-		second.setX(temp.getX());
-		second.setY(temp.getY());
+	public static void swap(final Point[] points, final int firstIndex, 
+			final int secondIndex) {
+		Point temp = points[firstIndex];
+		points[firstIndex] = points[secondIndex];
+		points[secondIndex] = temp;
 	}
 	
 	public static void sort(final Point[] points) {
 		for (int i = 0; i < points.length; i++) {
 			for (int j = points.length-1; j >= i; --j) {
 				if (points[i].getDistance() > points[j].getDistance()) {
-					swap(points[i], points[j]);
+					swap(points, i, j);
 				}
 			}
 		}
