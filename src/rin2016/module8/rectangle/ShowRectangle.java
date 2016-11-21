55668c5e9781bce00f6a13bcd7951e3380815dde
Lyuda Lyska <lucy.daehyun@gmail.com>
2016-11-21 15:50:51 +0200

    реализован класс для вывода на консоль прямоугольника

---
+++ src/rin2016/module8/rectangle/ShowRectangle.java
@@ -0,0 +1,32 @@
+package rin2016.module8.rectangle;
+
+public class ShowRectangle {
+	
+	public static void show(final Rectangle rectangle) {
+		if (rectangle != null) {
+			showHorizontalLine(rectangle);
+			showVerticallyLines(rectangle);
+			showHorizontalLine(rectangle);
+		}
+	}
+	
+	private static void showVerticallyLines(final Rectangle a) {
+		for (int j = 0; j < a.width()-2; j++) {
+			for (int i = 0; i < a.length(); i++) {
+				if (i == 0 || i == a.length() - 1) {
+					System.out.print("+");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+	
+	private static void showHorizontalLine(final Rectangle a) { 
+		for (int i = 0; i < a.length(); i++) {
+			System.out.print("+");
+		}
+		System.out.println();
+	}
+}
--- src/rin2016/module8/rectangle/Test.java
+++ src/rin2016/module8/rectangle/Test.java
@@ -3,14 +3,16 @@ package rin2016.module8.rectangle;
 public class Test {
 
 	public static void main(final String[] args) {
-		Rectangle a = new Rectangle(-5,3,0,0);
-		Rectangle b = new Rectangle(-2,5,3,2);
+		Rectangle a = new Rectangle(-8,3,0,-1);
+		Rectangle b = new Rectangle(-5,5,3,3);
 		
 		if (a.isIntersect(b)) {
 			System.out.println("Прямоугольники пересекаются!");
 		} else {
 			System.out.println("Прямоугольники не пересекаются!");
 		}
+		ShowRectangle.show(a);
+		
 	}
 
 }
